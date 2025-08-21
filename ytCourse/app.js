82c23d08077eb43041733682b77df8163b1f3fdb
Han <variety1174@gmail.com>
2025-08-21 21:55:48 +0530

    test

--- ytCourse/app.js
+++ ytCourse/app.js
@@ -1,16 +1,9 @@
-const http = require("http");
+const EventEmitter = require("events");
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.end("welcome to my homepage");
-  }
-  if (req.url === "/about") {
-    res.end("here is our short history");
-  }
-  res.end(`
-        <h1>Oops!</h1>
-        <p> We can't seem to find this page. </p>
-        <a href = "/">Back Home</a>`);
+const customEmitter = new EventEmitter();
+
+customEmitter.on("response", () => {
+  console.log(`data recieved`);
 });
 
-server.listen(5000);
+customEmitter.emit("response");
